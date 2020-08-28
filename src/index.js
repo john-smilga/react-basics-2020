b1e855c307bd48b594886bdae5445854640de0cb
john-smilga <johnsmilga@gmail.com>
2020-08-28 14:50:55 -0700

    17-jsx-rules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-// stateless functional component
-// always return JSX
+// JSX Rules
+// return single element
+// div / section / article or Fragment
+// use camelCase for html attribute
+// className instead of class
+// close every element
+// formatting
 
 function Greeting() {
   return (
@@ -11,13 +16,5 @@ function Greeting() {
     </div>
   );
 }
-
-// const Greeting = () => {
-//   return React.createElement(
-//     'div',
-//     {},
-//     React.createElement('h1', {}, 'hello world')
-//   );
-// };
 
 ReactDom.render(<Greeting />, document.getElementById('root'));
