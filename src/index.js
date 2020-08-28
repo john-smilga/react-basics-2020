526db82ea113df4224651448503a2bab2a8f3ebd
john-smilga <johnsmilga@gmail.com>
2020-08-28 15:58:48 -0700

    19-book-mini-project

--- src/index.js
+++ src/index.js
@@ -3,18 +3,36 @@ import ReactDom from 'react-dom';
 
 // Nested Components, React Tools
 
-function Greeting() {
+function BookList() {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 }
 
-const Person = () => <h2>john doe</h2>;
-const Message = () => {
-  return <p>this is my message</p>;
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+      <Title />
+      <Author />
+    </article>
+  );
 };
+const Image = () => (
+  <img
+    src='https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg'
+    alt=''
+  />
+);
 
-ReactDom.render(<Greeting />, document.getElementById('root'));
+const Title = () => <h1>I Love You to the Moon and Back</h1>;
+const Author = () => <h4>Amelia Hepworth</h4>;
+ReactDom.render(<BookList />, document.getElementById('root'));
