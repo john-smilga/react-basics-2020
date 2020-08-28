41ba48ac391c8cffc1ff30205c0d0b0b101f46b2
john-smilga <johnsmilga@gmail.com>
2020-08-28 14:12:03 -0700

    16-first-component-in-detail

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+// stateless functional component
+// always return JSX
+
 function Greeting() {
-  return <h4>this is john and this is my first component</h4>;
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  );
 }
+
+// const Greeting = () => {
+//   return React.createElement(
+//     'div',
+//     {},
+//     React.createElement('h1', {}, 'hello world')
+//   );
+// };
 
 ReactDom.render(<Greeting />, document.getElementById('root'));
