d16cc1aee1a30c4a60e67b875edca7715202ae7f
john-smilga <johnsmilga@gmail.com>
2020-08-29 10:24:27 -0700

    22-jsx-javascript

--- src/index.js
+++ src/index.js
@@ -7,36 +7,24 @@ function BookList() {
   return (
     <section className='booklist'>
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
-
+const author = 'Amelia Hepworth';
 const Book = () => {
+  const title = 'I Love You to the Moon and Back';
   return (
     <article className='book'>
-      <Image></Image>
-      <Title />
-      <Author />
+      <img
+        src='https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg'
+        alt=''
+      />
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
+      {/* <p>{let x = 6}</p> */}
+      <p>{6 + 6}</p>
     </article>
   );
 };
-const Image = () => (
-  <img
-    src='https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg'
-    alt=''
-  />
-);
 
-const Title = () => <h1>I Love You to the Moon and Back</h1>;
-const Author = () => (
-  <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}>
-    Amelia Hepworth
-  </h4>
-);
 ReactDom.render(<BookList />, document.getElementById('root'));
