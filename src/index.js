075d0faced1cfdc567b7947f582c00ca230ac9f8
john-smilga <johnsmilga@gmail.com>
2020-08-29 10:50:44 -0700

    24-props-destructuring

--- src/index.js
+++ src/index.js
@@ -3,26 +3,42 @@ import ReactDom from 'react-dom';
 
 // CSS
 import './index.css';
+const firstBook = {
+  img:
+    'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+  title: 'I Love You to the Moon and Back',
+  author: 'Amelia Hepworth',
+};
+const secondBook = {
+  img:
+    'https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg',
+  title: 'Our Class is a Family',
+  author: 'Shannon Olsen',
+};
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
-const author = 'Amelia Hepworth';
-const Book = () => {
-  const title = 'I Love You to the Moon and Back';
+
+const Book = ({ img, title, author }) => {
+  // const { img, title, author } = props;
   return (
     <article className='book'>
-      <img
-        src='https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg'
-        alt=''
-      />
+      <img src={img} alt='' />
       <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
-      {/* <p>{let x = 6}</p> */}
-      <p>{6 + 6}</p>
+      <h4>{author}</h4>
     </article>
   );
 };
