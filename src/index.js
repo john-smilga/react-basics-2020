aefa3a9654fd2217057f83d012410dfeae32a00e
john-smilga <johnsmilga@gmail.com>
2020-08-29 11:41:59 -0700

    28-key-spread-operator

--- src/index.js
+++ src/index.js
@@ -5,18 +5,21 @@ import ReactDom from 'react-dom';
 import './index.css';
 const books = [
   {
+    id: 1,
     img:
       'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
     title: 'I Love You to the Moon and Back',
     author: 'Amelia Hepworth',
   },
   {
+    id: 2,
     img:
       'https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg',
     title: 'Our Class is a Family',
     author: 'Shannon Olsen',
   },
   {
+    id: 3,
     img:
       'https://images-na.ssl-images-amazon.com/images/I/71e5m7xQd0L._AC_UL200_SR200,200_.jpg',
     title: 'The Vanishing Half: A Novel',
@@ -27,15 +30,15 @@ const books = [
 function BookList() {
   return (
     <section className='booklist'>
-      {books.map((book) => {
-        return <Book book={book}></Book>;
+      {books.map((book, index) => {
+        return <Book key={book.id} {...book}></Book>;
       })}
     </section>
   );
 }
 
-const Book = (props) => {
-  const { img, title, author } = props.book;
+const Book = ({ img, title, author }) => {
+  // const { img, title, author } = props;
 
   return (
     <article className='book'>
