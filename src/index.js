33027417d0129dab5b12fe6c17efcb47d07094a0
john-smilga <johnsmilga@gmail.com>
2020-08-29 11:34:39 -0700

    27-proper-list

--- src/index.js
+++ src/index.js
@@ -16,18 +16,26 @@ const books = [
     title: 'Our Class is a Family',
     author: 'Shannon Olsen',
   },
+  {
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/71e5m7xQd0L._AC_UL200_SR200,200_.jpg',
+    title: 'The Vanishing Half: A Novel',
+    author: 'Brit Bennett',
+  },
 ];
-const names = ['john', 'peter', 'susan'];
-const newNames = names.map((name) => {
-  return <h1>{name}</h1>;
-});
-console.log(newNames);
+
 function BookList() {
-  return <section className='booklist'>{newNames}</section>;
+  return (
+    <section className='booklist'>
+      {books.map((book) => {
+        return <Book book={book}></Book>;
+      })}
+    </section>
+  );
 }
 
 const Book = (props) => {
-  const { img, title, author } = props;
+  const { img, title, author } = props.book;
 
   return (
     <article className='book'>
